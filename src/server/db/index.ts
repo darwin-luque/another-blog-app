ebfad323fc216f0e639a6a22a5235d45c21dd6dc
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-04-25 15:16:45 -0600

    fix(db): force typing

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
-import { drizzle as drizzleLocal } from "drizzle-orm/postgres-js";
+import { drizzle as drizzleLocal, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import { sql } from "@vercel/postgres";
 import postgres from "postgres";
 import { env } from "@/env";
@@ -23,4 +23,4 @@ function getDB() {
   return drizzleLocal(conn, { schema });
 };
 
-export const db = getDB();
+export const db = getDB() as PostgresJsDatabase<typeof schema>;
