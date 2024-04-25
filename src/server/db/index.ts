52143f8ae5cbb42cf1ac481cc68321585a3619b8
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-04-25 14:32:52 -0600

    chore: setup db for vercel on prod

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,7 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
+import { drizzle as drizzleLocal } from "drizzle-orm/postgres-js";
+import { sql } from "@vercel/postgres";
 import postgres from "postgres";
-
 import { env } from "@/env";
 import * as schema from "./schema";
 
@@ -12,7 +13,14 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+function getDB() {
+  if (env.NODE_ENV === "production") {
+    return drizzleVercel(sql, { schema });
+  }
+  const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+  globalForDb.conn = conn;
 
-export const db = drizzle(conn, { schema });
+  return drizzleLocal(conn, { schema });
+};
+
+export const db = getDB();
